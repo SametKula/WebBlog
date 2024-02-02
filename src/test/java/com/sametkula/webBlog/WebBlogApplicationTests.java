d128cee9d8d4c393d986081d057e5ad02ee605d4
SametKula <sametkula@proton.me>
2024-02-03 01:48:12 +0300

    project created

---
+++ src/test/java/com/sametkula/webBlog/WebBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametkula.webBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
