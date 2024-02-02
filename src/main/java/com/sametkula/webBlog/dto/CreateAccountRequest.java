08187d4b8b52999b35481e11b3609968f737d123
SametKula <sametkula@proton.me>
2024-02-03 01:53:33 +0300

    Account(data): create and update request
    account create and update request added

---
+++ src/main/java/com/sametkula/webBlog/dto/CreateAccountRequest.java
@@ -0,0 +1,19 @@
+package com.sametkula.webBlog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAccountRequest {
+    private String username;
+    private String password;
+    private String email;
+
+}
---
+++ src/main/java/com/sametkula/webBlog/dto/UpdateAccountRequest.java
@@ -0,0 +1,31 @@
+package com.sametkula.webBlog.dto;
+
+import com.sametkula.webBlog.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateAccountRequest {
+    private UUID id;
+    private String username;
+    private String password;
+    private Date creationDate;
+    private String email;
+    private boolean isEmailEnabled;
+    private Integer totalPost;
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
+    private byte[] profilePicture;
+    private Set<Role> roles;
+}
